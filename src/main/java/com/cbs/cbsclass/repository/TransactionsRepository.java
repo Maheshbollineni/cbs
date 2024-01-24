9e3456022c69a019f1aa56db3a1e01aec9d90d30
audintelsaim <155418113+audintelsaim@users.noreply.github.com>
2024-01-24 09:52:37 +0530

    updated findByDate function logic

--- src/main/java/com/cbs/cbsclass/repository/TransactionsRepository.java
+++ src/main/java/com/cbs/cbsclass/repository/TransactionsRepository.java
@@ -13,6 +13,6 @@ import java.util.List;
 public interface TransactionsRepository extends CrudRepository<Transactions,Integer> {
     public List<Transactions> findByAccNo(long accno);
 
-    @Query("SELECT * FROM transactions WHERE tx_date = :currDate and tx_accno = :accno order by tx_date limit 1")
+    @Query("SELECT * FROM transactions WHERE tx_date <= :currDate and tx_accno = :accno order by tx_date desc limit 1")
     List<Transactions> findByDate(@Param("currDate") LocalDate currDate, @Param("tx_accno") long accno);
 }
