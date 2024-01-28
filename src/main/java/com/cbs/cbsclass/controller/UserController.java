a9cf81253372c0c4b76d92612a0eddfff2bba7a9
audintelsaim <155418113+audintelsaim@users.noreply.github.com>
2024-01-28 18:04:57 +0530

    corrected Cors error

--- src/main/java/com/cbs/cbsclass/controller/UserController.java
+++ src/main/java/com/cbs/cbsclass/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
 @RestController
-@CrossOrigin("origins = http://localhost:3000/")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class UserController {
 
     @Autowired
