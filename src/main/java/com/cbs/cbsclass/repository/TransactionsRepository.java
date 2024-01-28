e6e1f0ba07b7f04fd560bcf2301a2978651af888
audintelsaim <155418113+audintelsaim@users.noreply.github.com>
2024-01-28 11:31:47 +0530

    transaction repo

--- src/main/java/com/cbs/cbsclass/repository/TransactionsRepository.java
+++ src/main/java/com/cbs/cbsclass/repository/TransactionsRepository.java
@@ -13,9 +13,12 @@ import java.util.List;
 public interface TransactionsRepository extends CrudRepository<Transaction,Integer> {
     public List<Transaction> findByAccountno(long accno);
 
-    @Query("SELECT * FROM transactions WHERE tx_date <= :currDate and tx_accno = :accno order by tx_date desc limit 1")
+/*    @Query("SELECT t FROM transaction WHERE tx_at <= :currDate and tx_from = :tx_accno order by tx_at desc limit 1")
     List<Transaction> findByDate(@Param("currDate") LocalDate currDate, @Param("tx_accno") long accno);
 
-    @Query("SELECT * FROM transactions WHERE tx_date between :startDate and :endDate and tx_accno = :accno and interestamount is not null")
-    List<Transaction> findIfInterestCredited(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("tx_accno") long accno);
+    *//*@Query("SELECT t FROM transaction WHERE tx_from = :accno and interestamount is not null and tx_at between :startDate and :endDate")
+    List<Transaction> findIfInterestCredited(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("accno") long accno);*//*
+
+    @Query("SELECT t FROM Transaction t WHERE t.txFrom = :txFrom AND t.interestAmount IS NOT NULL AND t.txAt BETWEEN :startDate AND :endDate")
+    List<Transaction> findIfInterestCredited(@Param("txFrom") long txFrom, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);*/
 }
