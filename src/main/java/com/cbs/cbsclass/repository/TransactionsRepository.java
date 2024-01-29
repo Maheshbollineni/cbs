aad7c42f47f424d988ab7a4be63dd0cd90919a6a
audintelsaim <155418113+audintelsaim@users.noreply.github.com>
2024-01-29 12:37:51 +0530

    modified @Queries

--- src/main/java/com/cbs/cbsclass/repository/TransactionsRepository.java
+++ src/main/java/com/cbs/cbsclass/repository/TransactionsRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -14,12 +15,12 @@ public interface TransactionsRepository extends CrudRepository<Transaction,Integ
      List<Transaction> findByAccountno(long accno);
 //     @Query("SELECT t FROM Transaction t WHERE t.tx_at BETWEEN :dateTime.isAfter(startDate) and :dateTime.isAfter(endDate)")
 //     List<Transaction> findTransactionRangeDate(@Param("startDate") LocalDate startDate,@Param("endDate") LocalDate endDate);
-/*    @Query("SELECT t FROM transaction WHERE tx_at <= :currDate and tx_from = :tx_accno order by tx_at desc limit 1")
-    List<Transaction> findByDate(@Param("currDate") LocalDate currDate, @Param("tx_accno") long accno);
+    @Query("SELECT t FROM transaction t WHERE tx_at <= ?1 and tx_from = ?2 order by tx_at desc limit 1")
+    List<Transaction> findByDate(@Param("currDate") LocalDateTime currDate, @Param("tx_accno") long accno);
 
-    *//*@Query("SELECT t FROM transaction WHERE tx_from = :accno and interestamount is not null and tx_at between :startDate and :endDate")
-    List<Transaction> findIfInterestCredited(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("accno") long accno);*//*
+     /*@Query("SELECT t FROM transaction WHERE tx_from = :accno and interestamount is not null and tx_at between :startDate and :endDate")
+    List<Transaction> findIfInterestCredited(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("accno") long accno);*/
 
-    @Query("SELECT t FROM Transaction t WHERE t.txFrom = :txFrom AND t.interestAmount IS NOT NULL AND t.txAt BETWEEN :startDate AND :endDate")
-    List<Transaction> findIfInterestCredited(@Param("txFrom") long txFrom, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);*/
+    @Query("SELECT t FROM transaction t WHERE t.tx_from = ?1 AND t.interestamount IS NOT NULL AND t.tx_at BETWEEN ?2 AND ?3")
+    List<Transaction> findIfInterestCredited(@Param("txFrom") long txFrom, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
